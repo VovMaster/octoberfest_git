6d759910b4c45d8b29cad8a7bd2207ec7621863c
Vladimir <antonsimeonvladimir@gmail.com>
2017-08-04 08:24:05 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -23,8 +23,281 @@ $(document).ready(function(){
 	    }
 	}
 
+	$('.slider').slick({
+		dots: false,
+		infinite: true,
+		speed: 500,
+		autoplay: true,	  
+		fade: false,
+		slidesToShow: 5,
+		slidesToScroll: 5,
+		responsive: [
+		{
+		  breakpoint: 1024,
+		  settings: {
+		    slidesToShow: 4,
+		    slidesToScroll: 4,
+		  }
+		},
+		{
+		  breakpoint: 600,
+		  settings: {
+		    slidesToShow: 3,
+		    slidesToScroll: 3
+		  }
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+		    slidesToShow: 2,
+		    slidesToScroll: 2
+		  }
+		}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+		]
+	});
+
+
+
+
+
+
 
 });
+
+function initMap() {
+var myLatlng = new google.maps.LatLng(-27.4314157, 153.0330532);
+var map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: -27.4302157, lng: 153.0408532},
+  zoom: 16,
+  scrollwheel: false,
+  styles: [
+	    {
+	        "featureType": "all",
+	        "elementType": "labels.text.fill",
+	        "stylers": [
+	            {
+	                "saturation": 36
+	            },
+	            {
+	                "color": "#000000"
+	            },
+	            {
+	                "lightness": 40
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "all",
+	        "elementType": "labels.text.stroke",
+	        "stylers": [
+	            {
+	                "visibility": "on"
+	            },
+	            {
+	                "color": "#000000"
+	            },
+	            {
+	                "lightness": 16
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "all",
+	        "elementType": "labels.icon",
+	        "stylers": [
+	            {
+	                "visibility": "off"
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "administrative",
+	        "elementType": "geometry.fill",
+	        "stylers": [
+	            {
+	                "color": "#000000"
+	            },
+	            {
+	                "lightness": 20
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "administrative",
+	        "elementType": "geometry.stroke",
+	        "stylers": [
+	            {
+	                "color": "#000000"
+	            },
+	            {
+	                "lightness": 17
+	            },
+	            {
+	                "weight": 1.2
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "administrative.locality",
+	        "elementType": "geometry.fill",
+	        "stylers": [
+	            {
+	                "lightness": "37"
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "landscape",
+	        "elementType": "all",
+	        "stylers": [
+	            {
+	                "visibility": "on"
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "landscape",
+	        "elementType": "geometry",
+	        "stylers": [
+	            {
+	                "color": "#000000"
+	            },
+	            {
+	                "lightness": 20
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "poi",
+	        "elementType": "geometry",
+	        "stylers": [
+	            {
+	                "color": "#000000"
+	            },
+	            {
+	                "lightness": 21
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "road",
+	        "elementType": "all",
+	        "stylers": [
+	            {
+	                "visibility": "on"
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "road.highway",
+	        "elementType": "geometry.fill",
+	        "stylers": [
+	            {
+	                "color": "#000000"
+	            },
+	            {
+	                "lightness": 17
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "road.highway",
+	        "elementType": "geometry.stroke",
+	        "stylers": [
+	            {
+	                "color": "#000000"
+	            },
+	            {
+	                "lightness": 29
+	            },
+	            {
+	                "weight": 0.2
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "road.arterial",
+	        "elementType": "geometry",
+	        "stylers": [
+	            {
+	                "color": "#000000"
+	            },
+	            {
+	                "lightness": 18
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "road.local",
+	        "elementType": "geometry",
+	        "stylers": [
+	            {
+	                "color": "#000000"
+	            },
+	            {
+	                "lightness": 16
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "transit",
+	        "elementType": "geometry",
+	        "stylers": [
+	            {
+	                "color": "#000000"
+	            },
+	            {
+	                "lightness": 19
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "water",
+	        "elementType": "all",
+	        "stylers": [
+	            {
+	                "saturation": "10"
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "water",
+	        "elementType": "geometry",
+	        "stylers": [
+	            {
+	                "color": "#000000"
+	            },
+	            {
+	                "lightness": 17
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "water",
+	        "elementType": "geometry.fill",
+	        "stylers": [
+	            {
+	                "saturation": "-100"
+	            },
+	            {
+	                "lightness": "-100"
+	            }
+	        ]
+	    }
+	]
+});
+
+
+var goldStar = "images/logo.png"
+var marker = new google.maps.Marker({
+  position: myLatlng,
+  icon: goldStar,
+  map: map
+});
+}
 audiojs.events.ready(function() {
 	audiojs.createAll();
 });

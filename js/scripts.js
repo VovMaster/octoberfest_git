213a40ee2187bda91876727563871a0e5f248b74
Vladimir <antonsimeonvladimir@gmail.com>
2017-08-08 11:08:09 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -335,6 +335,70 @@ var marker = new google.maps.Marker({
   map: map
 });
 }
-audiojs.events.ready(function() {
-	audiojs.createAll();
-});
+
+
+// audiojs.events.ready(function() {
+// 	audiojs.createAll();
+// });
+
+$(function() { 
+    // Setup the player to autoplay the next track
+
+
+    var a = audiojs.createAll({
+      trackEnded: function() {
+        var next = $('ol li.playing').next();
+        if (!next.length) next = $('ol li').first();
+        next.addClass('playing').siblings().removeClass('playing');
+        audio.load($('a', next).attr('data-src'));
+        audio.play();
+      }
+    });
+    
+    // Load in the first track
+
+   	$('audio').each(function(i){
+
+
+    var audio = a[i];
+        first = $(this).closest('.audio-wrap').find('ol a').attr('data-src');
+    $(this).closest('.audio-wrap').find('ol li').first().addClass('playing');
+    audio.load(first);
+
+    // Load in a track on click
+    $(this).closest('.audio-wrap').find('ol li').click(function(e) {
+      e.preventDefault();
+      $(this).addClass('playing').siblings().removeClass('playing');
+      audio.load($('a', this).attr('data-src'));
+      audio.play();
+    });
+
+    $(this).closest('.audio-wrap').find('.prev-audio').click(function(e) {
+    	$('.prev-audio')
+    });
+
+
+    // Keyboard shortcuts
+    $(document).keydown(function(e) {
+      var unicode = e.charCode ? e.charCode : e.keyCode;
+         // right arrow
+      if (unicode == 39) {
+        var next = $('li.playing').next();
+        if (!next.length) next = $('ol li').first();
+        next.click();
+        // back arrow
+      } else if (unicode == 37) {
+        var prev = $('li.playing').prev();
+        if (!prev.length) prev = $('ol li').last();
+        prev.click();
+        // spacebar
+      } else if (unicode == 32) {
+        audio.playPause();
+      }
+    })
+
+
+
+
+  });
+  });
